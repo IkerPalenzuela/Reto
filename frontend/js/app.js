dfe6ed247e9babd412032ec917b04181d8b5e3f4
Iker Palenzuela <ikdgr@plaiaundi.net>
2026-01-20 10:32:41 +0100

    Commit edit index, css y js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,39 @@
-// CODIGO DEL CARRUSEL DE IMAGENES
+// MENU DE HAMBURGUESA Y NAVEGACION
+// Funcion para habrir y cerrar el menu
+function menu(){
+    let navegacion = document.getElementById('menu-principal');
+    navegacion.classList.toggle('mostrar')
+}
 
+// Función para ir a las secciones (llamada desde onclick="irA(...)")
+function irA(seccion) {
+    // PASO 1: Comprobar si el usuario está logueado.
+    // Como estamos simulando, miramos si hay un "token" guardado en el navegador.
+    // (Nota: Cuando hagas la parte del Login, tendrás que guardar este item)
+    let estoyLogueado = localStorage.getItem('auth_token'); // O la cookie de sesión
+
+    // PASO 2: Decidir a dónde ir
+    if (estoyLogueado) {
+        // --- SI ESTÁ LOGUEADO: Le llevamos a la página real ---
+        console.log("Usuario autorizado. Navegando a: " + seccion);
+
+        if (seccion === 'inicio') {
+            window.location.href = '/dashboard'; // El inicio de usuarios es el Dashboard
+        } else if (seccion === 'videojuegos') {
+            window.location.href = '/games';     // Página de listado de juegos
+        } else if (seccion === 'reseñas') {
+            window.location.href = '/reviews';   // Página de reseñas
+        } else if (seccion === 'favoritos') {
+            window.location.href = '/favorites'; // Página de favoritos
+        }
+
+    } else {
+        // --- SI NO ESTÁ LOGUEADO: Al Login ---
+        console.log("Usuario no registrado. Redirigiendo a Login...");
+        window.location.href = '/login';
+    }
+}
+// CODIGO DEL CARRUSEL DE IMAGENES
 let indice = 0; // Guardamos por qué foto vamos
 function mostrarImagen() {
     let imagenes = document.querySelectorAll('.imagen-carrusel'); // Cogemos todas las imagenes
@@ -30,15 +64,12 @@ setInterval(function() {
     moverCarrusel(1);
 }, 4000);
 
-
-// ============================================
-// 2. CARGAR RESEÑAS DESDE LA API
-// ============================================
+// CARGAR RESEÑAS DESDE LA API
 
 // Llamamos a la API de Laravel
 fetch('/api/reviews')
     .then(function(respuesta) {
-        return respuesta.json(); // Convertir a datos leibles
+        return respuesta.json(); // Convertimos la api en JSON
     })
     .then(function(datos) {
         let contenedor = document.getElementById('contenedor-reseñas');
