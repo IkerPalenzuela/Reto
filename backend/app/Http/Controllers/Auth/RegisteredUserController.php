2a36195e5523c91b7aff22c359af7332830c7b76
Iker Palenzuela <ikdgr@plaiaundi.net>
2026-02-10 13:35:17 +0100

    Commit para hacer cambios en la vista de register

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Profile; // <--- Importante
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -24,21 +25,29 @@ class RegisteredUserController extends Controller
 
     /**
      * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'], // Nuevo
+            'phone' => ['required', 'string', 'max:20'],    // Nuevo
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // Crear usuario
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+        ]);
+
+        // Crear perfil
+        Profile::create([
+            'user_id' => $user->id,
+            'surname' => $request->surname,
+            'phone' => $request->phone,
         ]);
 
         event(new Registered($user));
